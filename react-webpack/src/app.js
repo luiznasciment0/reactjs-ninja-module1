710384d360d61dc381f56b43a456f9988d45152e
Luiz Nascimento <luizp.nascimento@outlook.com>
2020-08-05 01:19:22 -0300

    first commit  and first look at react props

---
+++ react-webpack/src/app.js
@@ -0,0 +1,15 @@
+"use strict";
+import React from "react";
+import Title from "./title";
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <Title name="Luiz Nascimento" />
+      </div>
+    );
+  },
+});
+
+export default App;
